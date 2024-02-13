5811230f04d2e77da901566530bb35a05ac28e5d
Ritam Rakshit <ritamrakshit33@gmail.com>
2024-02-13 15:10:09 +0530

    training_pipeline updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,3 +1,4 @@
+from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
@@ -21,3 +22,7 @@ train_arr,test_arr = data_transformation.initiate_data_transformation(train_data
 
 model_trainer_obj = ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
+
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
